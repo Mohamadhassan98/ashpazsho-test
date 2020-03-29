f8b7d211b6cf5fc4d2c8f3935a01d1e3ee0192bd
Mohamadhassan <emohamadhassan@gmail.com>
2020-03-29 17:39:44 +0430

    Mobile Header fixed.

--- src/about-course/MobileCourseDetails.js
+++ src/about-course/MobileCourseDetails.js
@@ -4,7 +4,7 @@ import {useFont, useMargin, useWidth} from "../values/web";
 import Typography from "@material-ui/core/Typography";
 import clsx from "clsx";
 import Divider from "@material-ui/core/Divider";
-import {genericStyles, strings} from "../values/values";
+import {colors, genericStyles, strings} from "../values/values";
 import {makeStyles} from "@material-ui/styles";
 import Rating from "@material-ui/lab/Rating";
 import HiddenScroll from "../hiddenScroll";
@@ -13,6 +13,11 @@ import food1 from '../assets/food1.png';
 import food2 from '../assets/food2.png';
 import food3 from '../assets/food3.png';
 import {Paper} from "@material-ui/core";
+import chef from '../assets/chef.png';
+import calendar from '../assets/calendar.png';
+import license from '../assets/license.png';
+import teacher from '../assets/Teacher.png';
+import {useSize} from "../values/mobile";
 
 const useStyles = makeStyles(theme => ({
     divider: {
@@ -29,11 +34,23 @@ const useStyles = makeStyles(theme => ({
             marginInlineStart: '80px',
         },
     },
+    honorBullet: {
+        width: 6,
+        height: 6,
+        borderRadius: 3,
+        background: '#FF364A'
+    },
+    honor: {
+        fontSize: 12
+    }
 }));
+
+const colorStyles = makeStyles(theme => colors);
 
 export default function MobileCourseDetails() {
     const {pageTest1} = strings;
     const genericClasses = genericStyles();
+    const colorClasses = colorStyles();
     const classes = useStyles();
     return (
         <Grid container direction='column'>
@@ -62,11 +79,11 @@ export default function MobileCourseDetails() {
             </Grid>
             <Grid item>
                 <Typography>
-                    {pageTest1.aboutCourse}
+                    {pageTest1.learnThreeRecipes}
                 </Typography>
             </Grid>
             <HiddenScroll horizontal>
-                <Grid item container>
+                <Grid item container wrap='nowrap'>
                     <Grid item>
                         <FoodCard
                             background={food1}
@@ -92,7 +109,105 @@ export default function MobileCourseDetails() {
             </HiddenScroll>
             <Grid item>
                 <Paper>
-
+                    <Grid container direction='column'>
+                        <Grid item>
+                            <Paper elevation={0}>
+                                <Grid container direction='column'>
+                                    <Grid item container>
+                                        <Grid item container>
+                                            <Grid item>
+                                                <img src={chef} className={useSize(24, 24)} alt='chef'/>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className={useFont(14)}>
+                                                    {pageTest1.teacher}:
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography className={useFont(14, 'medium')}>
+                                                {pageTest1.teacher1}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item container>
+                                        <Grid item container>
+                                            <Grid item>
+                                                <img src={calendar} className={useSize(24, 24)} alt='calendar'/>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography className={useFont(14)}>
+                                                    {pageTest1.courseDate}:
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography className={useFont(14, 'medium')}>
+                                                {pageTest1.courseDate1}
+                                                <wbr/>
+                                                {pageTest1.courseTime1}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            </Paper>
+                        </Grid>
+                        <Grid item container>
+                            <Grid item container>
+                                <Grid item>
+                                    <img src={license} className={useSize(24, 24)} alt='license'/>
+                                </Grid>
+                                <Grid item>
+                                    <Typography className={useFont(16, 'medium')}>
+                                        {pageTest1.license}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid item>
+                                <Typography className={clsx([useFont(16, 'bold'), colorClasses.fontGreenColor])}>
+                                    {pageTest1.price1}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Paper>
+            </Grid>
+            <Grid item>
+                <Paper>
+                    <Grid container>
+                        <Grid item container direction='column'>
+                            <Grid item container>
+                                <Grid item>
+                                    <Typography>
+                                        {pageTest1.teacher}
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>
+                                        {pageTest1.teacher1}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                            <Grid item container>
+                                {pageTest1.teacherHonors.map((item, index) => (
+                                    <Grid item container key={index}>
+                                        <Grid item>
+                                            <div className={classes.honorBullet}
+                                            />
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography className={classes.honor}>
+                                                {item}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                ))}
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <img src={teacher} className={useSize(65, 65)} alt='teacher'/>
+                        </Grid>
+                    </Grid>
                 </Paper>
             </Grid>
         </Grid>
