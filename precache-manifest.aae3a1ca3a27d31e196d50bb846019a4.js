dd283542ec378f476fe2658069f7320969a8da72
Mohamadhassan <emohamadhassan@gmail.com>
2020-03-30 07:01:44 +0430

    Updates

---
+++ precache-manifest.aae3a1ca3a27d31e196d50bb846019a4.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e3ffdff888df3811bf8636bc31d298",
+    "url": "/ashpazsho-test/index.html"
+  },
+  {
+    "revision": "2120be39adc090f294b5",
+    "url": "/ashpazsho-test/static/css/main.9e21ff33.chunk.css"
+  },
+  {
+    "revision": "b4ca34d3a38c94d521bf",
+    "url": "/ashpazsho-test/static/js/2.4033a0a7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ashpazsho-test/static/js/2.4033a0a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2120be39adc090f294b5",
+    "url": "/ashpazsho-test/static/js/main.f072df2b.chunk.js"
+  },
+  {
+    "revision": "835e653667bf155987dc",
+    "url": "/ashpazsho-test/static/js/runtime-main.f213bdb0.js"
+  },
+  {
+    "revision": "5d86e75ed549612bd40352e5f50b3f1b",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum).5d86e75e.eot"
+  },
+  {
+    "revision": "a1233cc118c5257c1add132a786df256",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum).a1233cc1.woff"
+  },
+  {
+    "revision": "bab5e043e4fc4bcce51699f943003c85",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum).bab5e043.ttf"
+  },
+  {
+    "revision": "e8ef20357ee97e45a8d93cd1e0d1330f",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum).e8ef2035.woff2"
+  },
+  {
+    "revision": "3589a3ce688ae8bb4844695fc99486db",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Bold.3589a3ce.woff2"
+  },
+  {
+    "revision": "8f812ea61c00dbbd63226a4e5604e0c6",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Bold.8f812ea6.ttf"
+  },
+  {
+    "revision": "a97ff63f432049ada562b2d4cda681ff",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Bold.a97ff63f.eot"
+  },
+  {
+    "revision": "e836ee3ec82a8794c92cc132e6351695",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Bold.e836ee3e.woff"
+  },
+  {
+    "revision": "1affe2af4bb70abb35723371a8d8dbaf",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Light.1affe2af.woff"
+  },
+  {
+    "revision": "368618b75bce7491df78cfc6de038248",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Light.368618b7.eot"
+  },
+  {
+    "revision": "c0efd1108540ff4bfe02c234b41d5034",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Light.c0efd110.woff2"
+  },
+  {
+    "revision": "d7db9d342d2c3e35d74e1e7f8ebca034",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Light.d7db9d34.ttf"
+  },
+  {
+    "revision": "48ea858314dc430c179558f31b200aff",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Medium.48ea8583.ttf"
+  },
+  {
+    "revision": "7bae6b3eb09606070e44c655c6207a44",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Medium.7bae6b3e.eot"
+  },
+  {
+    "revision": "e537579c2de61c03c1fe84d9563a869c",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Medium.e537579c.woff2"
+  },
+  {
+    "revision": "eff85aebf1c79d1ccc332283bf604853",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_Medium.eff85aeb.woff"
+  },
+  {
+    "revision": "68f68e336b3f4cc86ba15ed64cce2ef4",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_UltraLight.68f68e33.woff"
+  },
+  {
+    "revision": "7f61560320d2a87556ce0ff336d683a6",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_UltraLight.7f615603.ttf"
+  },
+  {
+    "revision": "f2c32605bdf09d45da537577601ba410",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_UltraLight.f2c32605.eot"
+  },
+  {
+    "revision": "f4aa344c5358e278486ba5437e5057b0",
+    "url": "/ashpazsho-test/static/media/IRANSansWeb(FaNum)_UltraLight.f4aa344c.woff2"
+  },
+  {
+    "revision": "c05d633a7973e5822f96e4f4d5ab5ce8",
+    "url": "/ashpazsho-test/static/media/Recipe1.c05d633a.png"
+  },
+  {
+    "revision": "c75827eef2abb5d964ec83f014be279b",
+    "url": "/ashpazsho-test/static/media/Recipe2.c75827ee.png"
+  },
+  {
+    "revision": "d7c8625cc39e1bb44a4401bec0637416",
+    "url": "/ashpazsho-test/static/media/Recipe3.d7c8625c.png"
+  },
+  {
+    "revision": "c2adc2e860e3064788b109913738c1cc",
+    "url": "/ashpazsho-test/static/media/Teacher.c2adc2e8.png"
+  },
+  {
+    "revision": "86ec12fdd67fa7c0c623d46a95212d08",
+    "url": "/ashpazsho-test/static/media/contained-logo.86ec12fd.png"
+  },
+  {
+    "revision": "72b0a42b4e33887fbc4ecd66913302cc",
+    "url": "/ashpazsho-test/static/media/food1.72b0a42b.png"
+  },
+  {
+    "revision": "c5d2f114c0b8c40945f50bca32537063",
+    "url": "/ashpazsho-test/static/media/food2.c5d2f114.png"
+  },
+  {
+    "revision": "18cb1be0190ef810dc8cf67ac9f606ce",
+    "url": "/ashpazsho-test/static/media/food3.18cb1be0.png"
+  },
+  {
+    "revision": "0e0ebb6ad67555f31c31f9e0b920f54f",
+    "url": "/ashpazsho-test/static/media/logo.0e0ebb6a.png"
+  },
+  {
+    "revision": "787b775327452870f061149fca65a6b8",
+    "url": "/ashpazsho-test/static/media/mobileFooter.787b7753.png"
+  },
+  {
+    "revision": "56215243103695f4e42abfce9f94dc48",
+    "url": "/ashpazsho-test/static/media/webFooter.56215243.png"
+  }
+]);
